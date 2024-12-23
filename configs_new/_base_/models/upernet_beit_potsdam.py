971d6fc2676afd7db851c1b54693fd0c54ee9324
AI-Tianlong <839290771@qq.com>
2024-12-23 17:31:12 +0800

    20241223-浪潮-1

--- configs_new/_base_/models/upernet_beit_potsdam.py
+++ configs_new/_base_/models/upernet_beit_potsdam.py
@@ -49,18 +49,18 @@ model = dict(
     pretrained=None,
     data_preprocessor=data_preprocessor,
     backbone=dict(type=BEiT, ),
-    decode_head=dict(
-        type=UPerHead,
-        in_channels=[1024, 1024, 1024, 1024],
-        in_index=[0, 1, 2, 3],
-        pool_scales=(1, 2, 3, 6),
-        channels=1024,
-        dropout_ratio=0.1,
-        num_classes=19,
-        norm_cfg=norm_cfg,
-        align_corners=False,
-        loss_decode=dict(
-            type=CrossEntropyLoss, use_sigmoid=False, loss_weight=1.0)),
+    # decode_head=dict(
+    #     type=UPerHead,
+    #     in_channels=[1024, 1024, 1024, 1024],
+    #     in_index=[0, 1, 2, 3],
+    #     pool_scales=(1, 2, 3, 6),
+    #     channels=1024,
+    #     dropout_ratio=0.1,
+    #     num_classes=19,
+    #     norm_cfg=norm_cfg,
+    #     align_corners=False,
+    #     loss_decode=dict(
+    #         type=CrossEntropyLoss, use_sigmoid=False, loss_weight=1.0)),
     # auxiliary_head=dict(
     #     type=FCNHead,
     #     in_channels=1024,
--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -8,9 +8,11 @@ from .seg_tta import SegTTAModel
 
 from .atl_encoder_decoder_hyp import EncoderDecoder_hyp
 from .atl_encoder_decoder_multi_embedding import ATL_Multi_Embedding_EncoderDecoder
-
+from .atl_encoder_decoder_multi_embedding_multi_decoder import ATL_Multi_Embedding_Multi_Decoder_EncoderDecoder
+from .atl_encoder_decoder_Membedding_Sdecoder_stack_after_patch_embedding import ATL_Multi_Embedding_Single_Decoder_AfterPatchEmbedding_stack_EncoderDecoder
 __all__ = [
     'BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'SegTTAModel',
     'MultimodalEncoderDecoder', 'DepthEstimator','EncoderDecoder_hyp',
-    'ATL_Multi_Embedding_EncoderDecoder'
+    'ATL_Multi_Embedding_EncoderDecoder', 'ATL_Multi_Embedding_Multi_Decoder_EncoderDecoder',
+    'ATL_Multi_Embedding_Single_Decoder_AfterPatchEmbedding_stack_EncoderDecoder'
 ]
